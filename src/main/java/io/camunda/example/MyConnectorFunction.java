e486a43e76d3dc1f584ba050c1e4579116cbc89b
Lama Alosaimi <lamo.boos@hotmail.com>
2024-11-18 10:00:11 +0300

    initial commit

--- src/main/java/io/camunda/example/MyConnectorFunction.java
+++ src/main/java/io/camunda/example/MyConnectorFunction.java
@@ -14,18 +14,6 @@ import org.slf4j.LoggerFactory;
     name = "MYCONNECTOR",
     inputVariables = {"authentication", "message"},
     type = "io.camunda:template:1")
-@ElementTemplate(
-    id = "io.camunda.connector.Template.v1",
-    name = "Template connector",
-    version = 1,
-    description = "Describe this connector",
-    icon = "icon.svg",
-    documentationRef = "https://docs.camunda.io/docs/components/connectors/out-of-the-box-connectors/available-connectors-overview/",
-    propertyGroups = {
-      @ElementTemplate.PropertyGroup(id = "authentication", label = "Authentication"),
-      @ElementTemplate.PropertyGroup(id = "compose", label = "Compose")
-    },
-    inputDataClass = MyConnectorRequest.class)
 public class MyConnectorFunction implements OutboundConnectorFunction {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MyConnectorFunction.class);
